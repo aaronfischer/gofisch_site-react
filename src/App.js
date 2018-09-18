0c6d6d3c2fe0c3849d7973ead962209f0f6969c2
Aaron Fischer <aaron.fischer@caxiam.com>
2018-09-17 22:19:46 -0400

    Update lock and remove logo.svg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SwiperWrapper from './SwiperWrapper';
 import SwiperSlide from './SwiperSlide';
